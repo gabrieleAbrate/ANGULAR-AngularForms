18a059b89c4225119bafcc84428bdc789fdd19ea
Gabriele Abrate <gabriele.abrate.2005@gmail.com>
2023-11-28 19:37:04 +0100

    primo commit, anche se è presente un errore

--- AngularForms/src/app/template-forms/template-forms.component.ts
+++ AngularForms/src/app/template-forms/template-forms.component.ts
@@ -9,26 +9,28 @@ import { Appointment } from '../appointment';
 
 export class TemplateFormsComponent {
   model: Appointment = {
-    name: '',
-    mail: '',
-    dayOfTheWeek: 'Lunedì',
-    office: 'ufficio_A',
+    name: "",
+    mail: "",
+    dayOfTheWeek: "Lunedì",
+    office: "ufficio_A",
     application0: false,
     application1: false,
     application2: false
   };
 
-  daysOfTheWeek = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì'];
+  daysOfTheWeek = [
+    "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì"
+  ];
 
   availableOffices = [
-    {id: 'A', name: 'ufficio_A'},
-    {id: 'B', name: 'ufficio_B'},
-    {id: 'C', name: 'ufficio_C'}
+    {id:"A", name:"ufficio_A"},
+    {id:"B", name:"ufficio_B"},
+    {id:"C", name:"ufficio_C"}
   ];
 
   applications = [
-    {id: 0},
-    {id: 1},
-    {id: 2}
+    {id:0},
+    {id:1},
+    {id:2}
   ];
 }
